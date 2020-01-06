0159febfca31727b99c88fbd27d909ec08a032c8
Balint <balintvillei@gmail.com>
2020-01-06 18:42:25 +0100

    Create models and migrations

---
+++ database/migrations/2020_01_06_173517_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title')->unique();
+            $table->date('release_date');
+            $table->unsignedBigInteger('author_id');
+            $table->timestamps();
+
+            $table->foreign('author_id')->references('id')->on('authors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
